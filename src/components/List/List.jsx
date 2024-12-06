4ad8649d675f532e3d4121c60f1120491e32ea0b
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-12-06 23:41:37 +0000

    feat: add button to display number of emojis in List component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -5,10 +5,15 @@ const emojis = ["🚀", "🎶", "🎁", "🎉", "✨"];
 
 export function List() {
   return (
-    <ul className="list">
-      {emojis.map((emoji) => (
-        <Item key={emoji} emoji={emoji} />
-      ))}
-    </ul>
+    <>
+      <button onClick={() => alert(`Number of emoji: ${emojis.length}`)}>
+        Number of emoji
+      </button>
+      <ul className="list">
+        {emojis.map((emoji) => (
+          <Item key={emoji} emoji={emoji} />
+        ))}
+      </ul>
+    </>
   );
 }
