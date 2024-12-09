3d7e4b319f920d70e93d5e256f6f7b567663c7fa
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-12-09 23:58:18 +0000

    feat: add styled button component to List for improved UI

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,11 +1,22 @@
 import { Item } from "../Item/Item";
 import styles from "./List.module.css";
+import styled from "styled-components";
 
 const emojis = ["🚀", "🎶", "🎁", "🎉", "✨"];
+
+const Button = styled.button`
+  background-color: #f1356d;
+  color: white;
+  border: none;
+  padding: 0.5rem 1rem;
+  margin-bottom: 1rem;
+  cursor: pointer;
+`;
 
 export function List() {
   return (
     <>
+      <Button>Test</Button>
       <button
         className={styles.btn}
         onClick={() => alert(`Number of emoji: ${emojis.length}`)}
