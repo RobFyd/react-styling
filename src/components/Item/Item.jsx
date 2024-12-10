fc49c49484de0b64de5ba5e389f88e6e4775fa0d
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-12-10 23:53:10 +0000

    style: replace CSS styles with styled-components for Item component

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -10,22 +10,42 @@ const StyledItem = styled.li`
   overflow: hidden;
 `;
 
+const StyledEmoji = styled.span`
+  display: block;
+  margin-bottom: 42px;
+  font-size: 32px;
+  transition: transform 0.8s;
+`;
+
+const StyledButton = styled.button`
+  background: transparent;
+  border: solid 1px;
+  padding: 12px;
+  border-radius: 5px;
+  cursor: pointer;
+  width: 100px;
+  color: white;
+  transition: background 0.3s, color 0.3s;
+
+  &:hover {
+    color: salmon;
+    background: white;
+  }
+`;
+
 export function Item({ emoji }) {
   const [zoomed, setZoomed] = useState(false);
 
   return (
     <StyledItem>
-      <span className={`${styles.emoji} ${zoomed ? styles.zoomed : ""}`}>
-        {emoji}
-      </span>
-      <button
-        className={styles.btn}
+      <StyledEmoji>{emoji}</StyledEmoji>
+      <StyledButton
         onClick={() => {
           setZoomed((wasZoomed) => !wasZoomed);
         }}
       >
         {zoomed ? "Zoom Out" : "Zoom In"}
-      </button>
+      </StyledButton>
     </StyledItem>
   );
 }
