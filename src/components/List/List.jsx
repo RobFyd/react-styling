7cfe975d97a79f69944bb2c0d54902692304ce0a
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-12-10 00:35:26 +0000

    style: replace Button component with StyledButton for improved styling and hover effects

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,25 +4,29 @@ import styled from "styled-components";
 
 const emojis = ["🚀", "🎶", "🎁", "🎉", "✨"];
 
-const Button = styled.button`
-  background-color: #f1356d;
-  color: white;
-  border: none;
-  padding: 0.5rem 1rem;
-  margin-bottom: 1rem;
+const StyledButton = styled.button`
+  background: transparent;
+  border: 1px solid salmon;
+  border-radius: 5px;
+  padding: 10px 20px;
   cursor: pointer;
+  width: 100px;
+  color: salmon;
+  transition: background 0.3s;
+  margin: 30px;
+
+  &:hover {
+    background: salmon;
+    color: white;
+  }
 `;
 
 export function List() {
   return (
     <>
-      <Button>Test</Button>
-      <button
-        className={styles.btn}
-        onClick={() => alert(`Number of emoji: ${emojis.length}`)}
-      >
+      <StyledButton onClick={() => alert(`Number of emoji: ${emojis.length}`)}>
         Number of emoji
-      </button>
+      </StyledButton>
       <ul className={styles.list}>
         {emojis.map((emoji) => (
           <Item key={emoji} emoji={emoji} />
