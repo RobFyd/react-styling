70c2876ae343498160e1f19a3eb2ff63a497f455
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-12-12 00:31:04 +0000

    style: update Item component to accept className prop and create StyledListItem for margin adjustments in List component

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -18,11 +18,11 @@ const StyledEmoji = styled.span`
 `;
 //  transform: ${({ $zoomed }) => ($zoomed ? "scale(3)" : "scale(1)")}; // Alternative way to write the above line
 
-export function Item({ emoji }) {
+export function Item({ emoji, className }) {
   const [zoomed, setZoomed] = useState(false);
 
   return (
-    <StyledItem>
+    <StyledItem className={className}>
       <StyledEmoji $zoomed={zoomed}>{emoji}</StyledEmoji>
       <StyledButton
         onClick={() => {
